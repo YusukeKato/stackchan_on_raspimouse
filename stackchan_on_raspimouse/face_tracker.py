06248e3600d5d7a943e4c72cdb11daeebf938e9e
YusukeKato <yusuke.kato652284@gmail.com>
2026-05-16 16:19:28 +0900

    add cmd_vel publisher for raspimouse

--- stackchan_on_raspimouse/face_tracker.py
+++ stackchan_on_raspimouse/face_tracker.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Int32MultiArray
+from geometry_msgs.msg import TwistStamped
 import cv2
 import numpy as np
 
@@ -22,15 +23,23 @@ class FaceTracker(Node):
             Int32MultiArray,
             '/stackchan/joint_commands',
             10)
+        self.pub_cmd_vel = self.create_publisher(
+            TwistStamped,
+            '/cmd_vel',
+            10)
         # current stackchan motor angle
         self.current_yaw = 0
         self.current_pitch = 0
         # init OpenCV
         cascade_path = '/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
         self.face_cascade = cv2.CascadeClassifier(cascade_path)
-        # P Gain
-        self.kp_yaw = 1.5
-        self.kp_pitch = 1.5
+        # stackchan param
+        self.kp_yaw = 2.0
+        self.kp_pitch = 2.0
+        # raspimouse param
+        self.target_face_width = 90.0
+        self.kp_linear = 0.003
+        self.deadband = 5.0
         self.get_logger().info('Face Tracker Node has been started.')
 
     def joint_callback(self, msg):
@@ -48,6 +57,10 @@ class FaceTracker(Node):
         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
         height, width = img.shape[:2]
         center_x, center_y = width // 2, height // 2
+        # create cmd_vel msg
+        cmd_vel_msg = TwistStamped()
+        cmd_vel_msg.header.stamp = self.get_clock().now().to_msg()
+        cmd_vel_msg.header.frame_id = 'base_link'
         if len(faces) > 0:
             faces = sorted(faces, key=lambda x: x[2]*x[3], reverse=True)
             (x, y, w, h) = faces[0]
@@ -67,9 +80,20 @@ class FaceTracker(Node):
             cmd_msg = Int32MultiArray()
             cmd_msg.data = [target_yaw, target_pitch]
             self.pub_joint.publish(cmd_msg)
+            # calc cmd_vel
+            size_error = self.target_face_width - w
+            if abs(size_error) > self.deadband:
+                cmd_vel_msg.twist.linear.x = float(size_error * self.kp_linear)
+                cmd_vel_msg.twist.linear.x = max(-0.15, min(0.15, cmd_vel_msg.twist.linear.x))
+            else:
+                cmd_vel_msg.twist.linear.x = 0.0
             # debug
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.circle(img, (face_cx, face_cy), 2, (0, 0, 255), -1)
+        else:
+            cmd_vel_msg.twist.linear.x = 0.0
+        # publish cmd_vel
+        self.pub_cmd_vel.publish(cmd_vel_msg)
         # debug
         cv2.imshow("StackChan Face Tracking", img)
         cv2.waitKey(1)
